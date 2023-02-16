940f089600eebc172e47c97f218fd2fa637937e0
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-16 10:34:14 +0530

    Added: Calling UC3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LineComparison
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            CalculateLength Compare = new CalculateLength();
+            Compare.LengthLine();
+        }
+    }
+}
